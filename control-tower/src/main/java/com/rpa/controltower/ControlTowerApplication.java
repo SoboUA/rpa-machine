3f984eadf0f0f1caf56a2fd2eb5436800b34ee29
Oleh.Leush <12345cTc#>
2019-09-04 14:25:29 +0300

    fixed pom2

--- control-tower/src/main/java/com/rpa/controltower/ControlTowerApplication.java
+++ control-tower/src/main/java/com/rpa/controltower/ControlTowerApplication.java
@@ -1,24 +1,22 @@
 package com.rpa.controltower;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class ControlTowerApplication {
 
-	@Bean
-	public WebClient getWebClient(){
-		return WebClient.create();
-	}
+    @Bean
+    public WebClient getWebClient() {
+        return WebClient.create();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ControlTowerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ControlTowerApplication.class, args);
+    }
 
 }
