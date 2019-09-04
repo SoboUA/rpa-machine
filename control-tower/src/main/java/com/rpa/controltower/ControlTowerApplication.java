ab1b150955c01f03c32f197ccd93218d376a449d
Ruslan_Lelyk <ruslan_lelyk@epam.com>
2019-09-04 13:59:09 +0300

    add Eureka

--- control-tower/src/main/java/com/rpa/controltower/ControlTowerApplication.java
+++ control-tower/src/main/java/com/rpa/controltower/ControlTowerApplication.java
@@ -3,11 +3,13 @@ package com.rpa.controltower;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ControlTowerApplication {
 
 	@Bean
