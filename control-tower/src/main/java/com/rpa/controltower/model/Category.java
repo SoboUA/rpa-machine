db47d2ef0a560442143db79caf2ddcf11e051371
Roman_Sobolevskyi <Roman_Sobolevskyi@epam.com>
2019-09-14 19:09:36 +0300

    Add category

--- control-tower/src/main/java/com/rpa/controltower/model/Category.java
+++ control-tower/src/main/java/com/rpa/controltower/model/Category.java
@@ -2,9 +2,19 @@ package com.rpa.controltower.model;
 
 public enum Category {
 
-    FESTIVAL,
-    CONFERENCEC,
-    CONCERTS,
-    EXHIBITIONS,
-    OTHER;
+    FESTIVAL("Фестивалі"),
+    CONFERENCEC("Конференції"),
+    CONCERTS("Концерти"),
+    EXHIBITIONS("Виставки"),
+    OTHER("Інше");
+
+    private String value;
+
+    Category(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
